bcd2a2059005a8722d706a5548129c0d2b3ef2cb
Ziru Zou <jiyucoding@yahoo.com>
2023-01-30 18:48:37 -0500

    provide each button's onCLick handlers a function definition

--- src/components/MultiButton.js
+++ src/components/MultiButton.js
@@ -7,9 +7,10 @@ function MultiButton() {
 
   return (
     <div>
-      <button onClick={handleClick}>Button 1</button>
-      <button onClick={handleClick}>Button 2</button>
-      <button onClick={handleClick}>Button 3</button>
+      {/* provide a function definition, not a function invocation to the event listener */}
+      <button onClick={ () => handleClick(1)}>Button 1</button>
+      <button onClick={ () => handleClick(2)}>Button 2</button>
+      <button onClick={ () =>handleClick(3)}>Button 3</button>
     </div>
   );
 }
